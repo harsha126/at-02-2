d44517bb9865e7818b928de2a4daa8550ee894a0
harsha126 <harsha.sdm111@gmail.com>
2025-09-20 21:37:14 +0530

    feat: add PWA support with vite-plugin-pwa and include app icons.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "at-02-2",
+                short_name: "at-02-2",
+                description: "An app to communicate within the air force batch",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "/192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+    ],
 });
